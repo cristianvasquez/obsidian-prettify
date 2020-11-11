64d72896dabfd8253f882f1d6adfb761b9761d49
Cristian Vasquez <cristianvasquez@gmail.com>
2020-11-11 20:30:13 +0100

    version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-# obsidian-markdown-prettifier
-A markdown prettifier for obsidian
+# Markdown prettifier for Obsidian
+
+Uses [pretty-remarkable](https://github.com/jonschlinkert/pretty-remarkable) to fix and reformat ugly Markdown.
+
+The default hotkey is `Ctrl+Alt+L`.
+
+
+### Tables
+
+Normalizes table formatting.
+
+**Example**
+
+```markdown
+Before
+**A**|**B**|**C**
+|---:|:---|---|
+a |b |c
+x |y |z
+After
+```
+
+Is normalized to:
+
+```markdown
+Before
+
+| **A** | **B** | **C** | 
+| ---: | :--- | --- |
+| a | b | c |
+| x | y | z |
+
+After
+```
+
+### Unordered lists
+
+Normalizes unordered lists.
+
+**Example**
+
+```markdown
+* a
+* b
+  * c
+  * d
+    * e
+    * f
+```
+
+Is normalized to:
+
+```markdown
+* a
+* b
+  - c
+  - d
+    + e
+    + f
+```
+
+### Ordered lists
+
+Normalizes ordered lists.
+
+**Example**
+
+```markdown
+1. foo
+  a. aaa
+  b. bbb
+  c. ccc
+1. bar
+1. baz
+
+```
+
+Is normalized to:
+
+```markdown
+1. foo
+  a. aaa
+  b. bbb
+  c. ccc
+2. bar
+3. baz
+
+```
+
+
+## How to compile the plugin
+
+First, install the dependencies with
+
+```bash
+npm i
+```
+
+Then, you can compile the plugin with:
+
+```bash
+npm run build
+```
+
+This will create a `main.js` file in the project root. That is the entry point of your plugin.
---
+++ main.ts
@@ -0,0 +1,103 @@
+import {App, MarkdownView, Plugin, PluginSettingTab} from "obsidian";
+// @ts-ignore
+import {Remarkable} from "remarkable";
+// @ts-ignore
+import prettify from "pretty-remarkable"
+
+export default class MarkdownPrettifier extends Plugin {
+    setting: MarkdownPrettifierSettings;
+
+    onInit() {
+    }
+
+    async onload() {
+        console.log("Loading Markdown-Prettifier");
+
+        this.setting = (await this.loadData()) || new MarkdownPrettifierSettings();
+        this.addSettingTab(new MarkdownPrettifierSettingsTab(this.app, this));
+
+        this.addCommand({
+            id: "markdown-prettifier",
+            name: "Run",
+            callback: () => this.runPrettifier(),
+            hotkeys: [
+                {
+                    modifiers: ["Mod", "Alt"],
+                    key: "l",
+                },
+            ],
+        });
+    }
+
+    onunload() {
+        console.log("Unload Markdown-Prettifier");
+    }
+
+    runPrettifier() {
+        const view = this.app.workspace.activeLeaf.view;
+        if (view instanceof MarkdownView) {
+            // Do work here
+            const editor = view.sourceMode.cmEditor;
+
+            let text = editor.getSelection()
+
+            // Nothing selected, fall back to select all.
+            if (text==''){
+                editor.execCommand('selectAll')
+                text = editor.getSelection()
+            }
+
+            const newString = this.prettify(text);
+            editor.replaceSelection(newString, "start");
+        }
+    }
+
+    prettify(text: String): string {
+        return new Remarkable()
+            .use(prettify)
+            .render(text);
+    }
+}
+
+class MarkdownPrettifierSettings {
+    // firstNumber = 0;
+    // secondNumber = 5;
+}
+
+class MarkdownPrettifierSettingsTab extends PluginSettingTab {
+    plugin: MarkdownPrettifier;
+
+    constructor(app: App, plugin: MarkdownPrettifier) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const {containerEl} = this;
+        const settings = this.plugin.setting;
+        // new Setting(containerEl)
+        //     .setName("First setting")
+        //     .setDesc(
+        //         "Explanation for the first setting."
+        //     )
+        //     .addText((text) =>
+        //         text.setValue(String(settings.firstNumber)).onChange((value) => {
+        //             if (!isNaN(Number(value))) {
+        //                 settings.firstNumber = Number(value);
+        //                 this.plugin.saveData(settings);
+        //             }
+        //         })
+        //     );
+        // new Setting(containerEl)
+        //     .setName("Second number")
+        //     .setDesc("I don't know yet the purpose of this second number.")
+        //     .addText((text) =>
+        //         text.setValue(String(settings.firstNumber)).onChange((value) => {
+        //             if (!isNaN(Number(value))) {
+        //                 settings.secondNumber = Number(value);
+        //                 this.plugin.saveData(settings);
+        //             }
+        //         })
+        //     );
+    }
+}
