37c2530590b8792dbabe228b284da39939d0d556
Karel Wintersky <karel.wintersky@yandex.ru>
2021-04-12 20:33:15 +0300

    0.0.9
    * [+] added run-script `build_dev`, build development version with sourcemap
    * [*] modified run-script `dev`, added config param for building sourcemap
    * [*] modified `rollup.config.js`, build sourcemap only if `--configDebug` param defined

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,24 @@ import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
-export default {
-    input: './src/main.ts',
-    output: {
-        dir: '.',
-        sourcemap: 'inline',
-        format: 'cjs',
-        exports: 'default'
-    },
-    external: ['obsidian'],
-    plugins: [
-        json(),
-        typescript(),
-        nodeResolve({browser: true}),
-        commonjs(),
-    ]
+export default commandLineArgs => {
+    const isDebug = commandLineArgs.configDebug === true ? 'inline' : false;
+    delete commandLineArgs.input;
+    return {
+        input: './src/main.ts',
+        output: {
+            dir: '.',
+            sourcemap: isDebug,
+            format: 'cjs',
+            exports: 'default'
+        },
+        external: ['obsidian'],
+        plugins: [
+            json(),
+            typescript(),
+            nodeResolve({browser: true}),
+            commonjs(),
+        ]
+    }
 };
+
